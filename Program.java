9cada46406dbb333ada590d1b18b81ba0d178c57
Crazy767 <sascha.scotin2011@list.ru>
2024-05-30 16:03:12 +0300

    done

--- Program.java
+++ Program.java
@@ -187,7 +187,7 @@ public class Program {
 
     public static FileWriter fileWriter;
     public static PrintWriter printWriter;
-    public static void main(String[] args) throws IOException {
+    public static void mainn(String[] args) throws IOException {
        JFrame frame = new JFrame("Demo");
        frame.add(new TicTacToePanel(new GridLayout(3,3)));
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
