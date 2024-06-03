8004b31ece320c18c56bb7c731407ccecf009624
Frenetz <xodyrev.fedya@mail.ru>
2024-06-03 15:09:03 +0300

    fix tests

--- src/main/java/org.example/App.java
+++ src/main/java/org.example/App.java
@@ -6,86 +6,70 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.apache.commons.io.FileUtils;
 
-import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.net.URL;
 import java.time.Duration;
+import java.util.List;
 
 public class App {
-    // Функция для инициализации WebDriver
-    private static WebDriver инициализироватьWebDriver() {
-        // Установка пути к ChromeDriver
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Администратор\\Desktop\\lab7.2\\chromedriver-win64\\chromedriver.exe");
-        // Инициализация ChromeDriver
+
+    private static WebDriver initializationOfWebComponent() {
+        String pathToDriver = "C:\\Users\\Администратор\\Desktop\\lab7.2\\chromedriver-win64\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", pathToDriver);
         return new ChromeDriver();
     }
 
-    // Функция для заполнения формы на веб-сайте
-    private static void заполнитьФорму(WebDriver driver) throws Exception {
-        // Переход на веб-сайт
-        driver.get("http://www.papercdcase.com/index.php");
-        // Настройка WebDriverWait
-        WebDriverWait webWait = new WebDriverWait(driver, Duration.ofSeconds(3));
+    private static void createAndGetForm(WebDriver drForWeb) throws Exception {
+        drForWeb.get("http://www.papercdcase.com/index.php");
+        WebDriverWait waiterWhite = new WebDriverWait(drForWeb, Duration.ofSeconds(3));
 
-        // Локаторы для элементов формы
-        By artistFieldLocator = By.xpath("/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[1]/td[2]/input");
-        By titleFieldLocator = By.xpath("/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[2]/td[2]/input");
-        By paperRadioButtonLocator = By.xpath("/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[5]/td[2]/input[2]");
-        By typeRadioButtonLocator = By.xpath("/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[4]/td[2]/input[2]");
-        By generateButtonLocator = By.xpath("/html/body/table[2]/tbody/tr/td[1]/div/form/p/input");
+        List<WebElement> elemForInput = drForWeb.findElements(By.tagName("input"));
+        WebElement optionSinger = elemForInput.get(0);
+        WebElement optionForName = elemForInput.get(1);
+        WebElement variatyForPaper = elemForInput.get(6);
+        WebElement variaryForType = elemForInput.get(5);
+        WebElement button = elemForInput.get(elemForInput.size() - 1);
 
-        // Заполнение поля "Исполнитель"
-        WebElement artistInput = webWait.until(ExpectedConditions.visibilityOfElementLocated(artistFieldLocator));
-        artistInput.sendKeys("Dua Lipa");
+        optionSinger.sendKeys("Dua Lipa");
+        optionForName.sendKeys("Future Nostalgia");
 
-        // Заполнение поля "Название"
-        WebElement titleInput = webWait.until(ExpectedConditions.visibilityOfElementLocated(titleFieldLocator));
-        titleInput.sendKeys("Future Nostalgia");
-
-        // Заполнение списка композиций
-        String[] trackList = {"Future Nostalgia", "Physical", "Hallucinate", "Don't start Now", "Levitating", "Love Again", "Cool", "Pretty Please", "Break my heart"};
-        int index = 0;
-        while (index < trackList.length) {
-            By trackFieldLocator = By.xpath("/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[3]/td[2]/table/tbody/tr/td[1]/table/tbody/tr[" + (index + 1) + "]/td[2]/input");
-            WebElement trackInput = webWait.until(ExpectedConditions.visibilityOfElementLocated(trackFieldLocator));
-            trackInput.sendKeys(trackList[index]);
-            index++;
+        List<WebElement> ElemForSong = drForWeb.findElements(By.xpath("//tr[td[1]/text()='Track']]/td[2]/input"));
+        String[] compositions = {"Future Nostalgia", "Physical", "Hallucinate", "Don't Start Now", "Levitating", "Love Again", "Cool", "Pretty Please", "Break My Heart"};
+        for (int i = 0; i < compositions.length; i++) {
+            ElemForSong.get(i).sendKeys(compositions[i]);
         }
-        WebElement paperOption = webWait.until(ExpectedConditions.elementToBeClickable(paperRadioButtonLocator));
-        paperOption.click();
-        WebElement typeOption = webWait.until(ExpectedConditions.elementToBeClickable(typeRadioButtonLocator));
-        typeOption.click();
-        WebElement createButton = webWait.until(ExpectedConditions.elementToBeClickable(generateButtonLocator));
-        createButton.click();
+
+        variatyForPaper.click();
+        variaryForType.click();
+        button.click();
+
+        waiterWhite.until(ExpectedConditions.numberOfWindowsToBe(2));
+        drForWeb.switchTo().window(drForWeb.getWindowHandles().toArray(new String[0])[1]);
     }
 
-    // Функция для загрузки сгенерированного PDF-файла
-    private static void загрузитьPDF(WebDriver driver) throws Exception {
-        // Переключение на новое окно
-        for (String handle : driver.getWindowHandles()) {
-            driver.switchTo().window(handle);
+    private static void saveWalterWhiteCom(WebDriver drForWeb) throws Exception {
+        String pdfUrl = drForWeb.getCurrentUrl();
+        try (InputStream inPunk = new URL(pdfUrl).openStream(); FileOutputStream outPunk = new FileOutputStream("cd.pdf")) {
+            byte[] bruce = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inPunk.read(bruce)) != -1) {
+                outPunk.write(bruce, 0, bytesRead);
+            }
         }
-        // Получение ссылки для скачивания
-        String downloadLink = driver.getCurrentUrl();
-        // Загрузка PDF-файла
-        File outputFile = new File("cd.pdf");
-        FileUtils.copyURLToFile(new URL(downloadLink), outputFile);
     }
 
     public static void main(String[] args) {
-        WebDriver driver = инициализироватьWebDriver();
+        WebDriver drForWeb = initializationOfWebComponent();
         try {
-            заполнитьФорму(driver);
-            загрузитьPDF(driver);
-        } catch (Exception ex) {
-            // Вывод сообщения об ошибке
-            System.out.println("Ошибка");
-            // Вывод деталей исключения
-            System.out.println(ex.toString());
+            createAndGetForm(drForWeb);
+            saveWalterWhiteCom(drForWeb);
+        } catch (Exception e) {
+            System.err.println("An error occurred: " + e.getMessage());
+            e.printStackTrace();
         } finally {
-            // Закрытие экземпляра WebDriver
-            driver.quit();
+            drForWeb.quit();
         }
     }
 }
