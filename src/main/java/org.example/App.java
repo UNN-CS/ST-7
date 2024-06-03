864170910f74fa760ccfcbd98fcb6e4f52733e66
ZhatkinVyacheslav <90867068+ZhatkinVyacheslav@users.noreply.github.com>
2024-06-03 23:56:55 +0300

    FIX

--- src/main/java/org.example/App.java
+++ src/main/java/org.example/App.java
@@ -1,66 +1,51 @@
-package org.example;
-
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\fames\\OneDrive\\Рабочий стол\\lessons\\chrome-win64\\chrome-win64\\chrome.exe");
-        WebDriver webDriver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+public class App {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chrome-win64\\chrome-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         try {
-            webDriver.get("http://www.papercdcase.com/index.php");
-            String artistName = "Big Baby Tape, kizaru";
-            String albumTitle = "BANDANA I";
-            String[] tracks = {
-                    "99 Problems",
-                    "So Icy Nihao",
-                    "Big Tymers",
-                    "Dirrt",
-                    "Ladidadida",
-                    "Million",
-                    "5 Nights Crazy",
-                    "Errbody Sleeping",
-                    "Mama Makusa",
-                    "Bandana",
-                    "Slip & Slide",
-                    "Andrew Story",
-                    "Bon Voyage",
-                    "Ride Or Die",
-            };
+            driver.get("https://www.calculator.net/password-generator.html");
 
-            WebElement artist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[1]/td[2]/input")));
-            artist.sendKeys(artistName);
+        WebElement artistField = driver.findElement(By.name("artist"));
+        artistField.sendKeys("Konec solnechnih dney");
 
-            WebElement title = webDriver.findElement(By.xpath("/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[2]/td[2]/input"));
-            title.sendKeys(albumTitle);
+        WebElement titleField = driver.findElement(By.name("title"));
+        titleField.sendKeys("Osen");
 
-            for (int i = 0; i < tracks.length; i++) {
-                int row = (i < 8) ? i + 1 : i - 7;
-                int col = (i < 8) ? 1 : 2;
-                String xpath = String.format("/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[3]/td[2]/table/tbody/tr/td[%d]/table/tbody/tr[%d]/td[2]/input", col, row);
-                WebElement track = webDriver.findElement(By.xpath(xpath));
-                track.sendKeys(tracks[i]);
-            }
+        WebElement tracksField = driver.findElement(By.name("tracks"));
+        tracksField.sendKeys("Deti Nemoy Strany\nHoloda\nUyedu daleko\nOsen\nKuda katitca moya golova");
 
-            WebElement typeJewelcase = webDriver.findElement(By.xpath("/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[4]/td[2]/input[2]"));
-            typeJewelcase.click();
-            WebElement PaperA4 = webDriver.findElement(By.xpath("/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[5]/td[2]/input[2]"));
-            PaperA4.click();
+        WebElement formatA4 = driver.findElement(By.xpath("//input[@value='a4']"));
+        formatA4.click();
 
-            WebElement btn = webDriver.findElement(By.xpath("/html/body/table[2]/tbody/tr/td[1]/div/form/p/input"));
-            btn.submit();
-        } catch (Exception e) {
-            System.out.println("Error");
-            System.out.println(e.toString());
-        }
+        WebElement jewelCase = driver.findElement(By.xpath("//input[@value='jewel']"));
+        jewelCase.click();
+
+        WebElement btnSubmit = driver.findElement(By.name("submit"));
+        btnSubmit.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(@href, 'pdf')]")));
+
+        WebElement pdfLink = driver.findElement(By.xpath("//a[contains(@href, 'pdf')]"));
+        pdfLink.click();
+
+        Thread.sleep(5000);
+
+    } catch (Exception e) {
+        System.out.println("Error");
+        System.out.println(e.toString());
+    } finally {
+        driver.quit();
+    }
     }
 }
