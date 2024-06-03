57c2bb8467346e59ca4d51981161c4a571035bd0
Marissa696 <mrnsrtv@gmail.com>
2024-06-03 15:27:20 +0300

    fix tests

--- src/main/java/org.example/App.java
+++ src/main/java/org.example/App.java
@@ -4,72 +4,78 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.util.*;
 
-public class App {
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-    static Map<String, String> fieldXpaths = new LinkedHashMap<>() {{
-        put("ArtistName", "/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[1]/td[2]/input");
-        put("AlbumTitle", "/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[2]/td[2]/input");
-
-        for (int i = 1; i <= 5; i++) {
-            put("Song" + i, "/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[3]/td[2]/table/tbody/tr/td[1]/table/tbody/tr[" + i + "]/td[2]/input");
-        }
-
-        for (int i = 1; i <= 5; i++) {
-            put("Song" + (i + 5), "/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[3]/td[2]/table/tbody/tr/td[2]/table/tbody/tr[" + i + "]/td[2]/input");
-        }
-
-        put("JewelCase", "/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[4]/td[2]/input[2]");
-        put("A4Paper", "/html/body/table[2]/tbody/tr/td[1]/div/form/table/tbody/tr[5]/td[2]/input[2]");
-        put("SubmitButton", "/html/body/table[2]/tbody/tr/td[1]/div/form/p/input");
-    }};
-
-    static Map<String, String> inputData = new LinkedHashMap<>() {{
-        put("ArtistName", "Katy Perry");
-        put("AlbumTitle", "Teenage Dream");
-
-        String[] trackList = {
-            "Teenage Dream", "California Gurls", "Peacock", "The One That Got Away", "Pearl",
-            "Not Like the Movies", "Last Friday Night", "Firework", "Circle the Drain", "Who Am I Living For?"
-        };
-        for (int i = 1; i <= 10; i++) {
-            put("Song" + i, trackList[i - 1]);
-        }
-    }};
-
-    static String targetURL = "http://www.papercdcase.com/index.php";
-    static String chromeDriverPath = "D:\\Marina\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe";
+public class MainApp {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "/path/to/your/chromedriver");
+        WebDriver webConfigurator = new ChromeDriver();
+
+        Map<String, String> factElem = new LinkedHashMap<String, String>() {{
+            put("artistInput", "//input[@name='artist']");
+            put("albumInput", "//input[@name='album']");
+
+            int iter1 = 1;
+            while (iter1 <= 5) {
+                put("track" + iter1, "//table[@class='noBorder']/tbody/tr/td[1]/table/tbody/tr[" + iter1 + "]/td[2]/input");
+                iter1++;
+            }
+
+            int iter2 = 1;
+            while (iter2 <= 5) {
+                put("track" + (iter2 + 5), "//table[@class='noBorder']/tbody/tr/td[2]/table/tbody/tr[" + iter2 + "]/td[2]/input");
+                iter2++;
+            }
+
+            put("jewelCaseCheckbox", "//input[@value='Jewel Case']");
+            put("a4PaperCheckbox", "//input[@value='A4 Paper']");
+            put("submitButton", "//input[@type='submit']");
+        }};
+
+        Map<String, String> ValForIn = new LinkedHashMap<String, String>() {{
+            put("artistInput", "Katy Perry");
+            put("albumInput", "Teenage Dream");
+
+            String[] albumSongs = {
+                "Teenage Dream", "California Gurls", "Peacock", "The One That Got Away", "Pearl",
+                "Not Like the Movies", "Last Friday Night", "Firework", "Circle the Drain", "Who Am I Living For?"
+            };
+            int iter3 = 1;
+            while (iter3 <= 10) {
+                put("track" + iter3, albumSongs[iter3 - 1]);
+                iter3++;
+            }
+        }};
+
+        String LinkForApplication = "http://www.papercdcase.com/index.php";
 
         try {
-            driver.get(targetURL);
+            webConfigurator.get(LinkForApplication);
 
-            for (Map.Entry<String, String> fieldEntry : inputData.entrySet()) {
-                String fieldKey = fieldEntry.getKey();
-                String fieldValue = fieldEntry.getValue();
-                
-                if (fieldKey.startsWith("Song") && fieldValue.isEmpty()) {
+            for (Map.Entry<String, String> EntIn : ValForIn.entrySet()) {
+                String OpenIn = EntIn.getKey();
+                String ValIn = EntIn.getValue();
+
+                if (OpenIn.startsWith("track") && ValIn.isEmpty()) {
                     continue;
                 }
 
-                WebElement inputField = driver.findElement(By.xpath(fieldXpaths.get(fieldKey)));
-                inputField.sendKeys(fieldValue);
-
-                Thread.sleep(100);
+                WebElement inputField = webConfigurator.findElement(By.xpath(factElem.get(OpenIn)));
+                inputField.sendKeys(ValIn);
             }
 
-            driver.findElement(By.xpath(fieldXpaths.get("JewelCase"))).click();
-            driver.findElement(By.xpath(fieldXpaths.get("A4Paper"))).click();
-            driver.findElement(By.xpath(fieldXpaths.get("SubmitButton"))).click();
+            webConfigurator.findElement(By.xpath(factElem.get("jewelCaseCheckbox"))).click();
+            webConfigurator.findElement(By.xpath(factElem.get("a4PaperCheckbox"))).click();
+            webConfigurator.findElement(By.xpath(factElem.get("submitButton"))).click();
+
         } catch (Exception e) {
-            System.out.println("An error occurred:");
+            System.out.println("Возникли следующие ошибки:");
             System.out.println(e.getMessage());
         } finally {
-            driver.quit();
+            webConfigurator.quit();
         }
     }
 }
